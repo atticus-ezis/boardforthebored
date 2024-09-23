1214eadf0fa6f1ef7ddd34cc9967886544c19b28
Atticus Ezis <atticus.ezis@gmail.com>
2024-09-23 17:12:28 -0600

    added template styling and test_index.html

--- board4thebored/views.py
+++ board4thebored/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'index.html') 
+
+def test_index_view(request):
+    return render(request, 'test_index.html')
