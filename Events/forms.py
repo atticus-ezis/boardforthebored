dea1febafb7f116ad0e3f7f134cf7c7bb5b9310b
Atticus Ezis <atticus.ezis@gmail.com>
2024-10-14 21:06:44 -0700

    added carousel

--- Events/forms.py
+++ Events/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
+# create form for city search 
+
 class CitySearch(forms.Form):
     city = forms.CharField(
         max_length=100,
         label='',
-        widget=forms.TextInput(attrs={
-            'class': 'custom-search-bar', 
+        widget=forms.TextInput(attrs={  # create widget to add class styling 
+            'class': 'custom-search-bar', # round corners, flex grow, margin etc 
             'placeholder': 'Enter City',
         })
     )
